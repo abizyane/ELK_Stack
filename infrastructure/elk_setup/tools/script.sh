e66092a3327d386d8933ef6326fa3941e770c816
Achraf <abizyane@student.1337.ma>
2024-12-21 14:27:58 +0100

    Fix: Fixed the data flow

--- infrastructure/elk_setup/tools/script.sh
+++ infrastructure/elk_setup/tools/script.sh
@@ -1,37 +1,74 @@
 #!/bin/bash
 
-if [ x${ELASTIC_PASSWORD} == x ]; then
-  echo "Set the ELASTIC_PASSWORD environment variable in the .env file"
-  exit 1
-elif [ x${KIBANA_PASSWORD} == x ]; then
-  echo "Set the KIBANA_PASSWORD environment variable in the .env file"
-  exit 1
+echo "Setting up certificates ..."
+if [ ! -f config/certs/ca/ca.crt ]; then
+    echo "Creating Authority Certificate"
+    bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip
+    unzip -o -q config/certs/ca.zip -d config/certs
+    rm config/certs/ca.zip
 fi
 
-if [ ! -f config/certs/ca.zip ]; then
-  echo "Creating CA"
-  bin/elasticsearch-certutil ca --silent --pem -out config/certs/ca.zip
-  unzip config/certs/ca.zip -d config/certs
+if [ ! -f config/certs/elasticsearch/elasticsearch.crt ]; then
+    echo "Creating certificates"
+    echo '{
+      "instances": [
+        {
+          "name": "elasticsearch",
+          "dns": ["elasticsearch", "localhost"],
+          "ip": ["127.0.0.1"]
+        },
+        {
+          "name": "kibana",
+          "dns": ["kibana", "localhost"],
+          "ip": ["127.0.0.1"]
+        }
+      ]
+    }' > config/certs/ins.yml
+    bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/ins.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key
+    unzip -o -q config/certs/certs.zip -d config/certs
+    rm config/certs/certs.zip config/certs/ins.yml
 fi
 
-if [ ! -f config/certs/certs.zip ]; then
-  echo "Creating certs"
-  echo -ne "instances:\n  - name: elasticsearch\n    dns:\n      - elasticsearch\n      - localhost\n    ip:\n      - 127.0.0.1\n  - name: kibana\n    dns:\n      - kibana\n      - localhost\n    ip:\n      - 127.0.0.1\n" > config/certs/instances.yml
-  bin/elasticsearch-certutil cert --silent --pem -out config/certs/certs.zip --in config/certs/instances.yml --ca-cert config/certs/ca/ca.crt --ca-key config/certs/ca/ca.key
-  unzip config/certs/certs.zip -d config/certs
-fi
-
-echo "Setting file permissions"
 chown -R root:root config/certs
 find . -type d -exec chmod 750 {} \;
 find . -type f -exec chmod 640 {} \;
 
-echo "Waiting for Elasticsearch availability"
-until curl -s --cacert config/certs/ca/ca.crt https://elasticsearch:9200 | grep -q "missing authentication credentials"; do sleep 30; done
+echo "Waiting for Elasticsearch ..."
+until curl -s --cacert config/certs/ca/ca.crt https://elasticsearch:9200 | grep -q "missing authentication credentials"; do sleep 10; done
 
 echo "Setting kibana_system password"
-until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 10; done
+until curl -s -X POST --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_security/user/kibana_system/_password -d "{\"password\":\"${KIBANA_PASSWORD}\"}" | grep -q "^{}"; do sleep 5; done
 
-rm -rf config/certs/ca.zip config/certs/certs.zip config/certs/instances.yml
+echo "Setting ILM policy"
+curl -s -X PUT --cacert config/certs/ca/ca.crt -u "elastic:${ELASTIC_PASSWORD}" -H "Content-Type: application/json" https://elasticsearch:9200/_ilm/policy/filebeat -d '
+{
+  "policy": {
+    "phases": {
+      "hot": {
+        "min_age": "0ms",
+        "actions": {
+          "rollover": {
+            "max_age": "7d",
+            "max_size": "5gb"
+          }
+        }
+      },
+      "cold": {
+        "min_age": "3d",
+        "actions": {
+          "readonly": {}
+        }
+      },
+      "delete": {
+        "min_age": "5d",
+        "actions": {
+          "delete": {
+            "delete_searchable_snapshot": true
+          }
+        }
+      }
+    }
+  }
+}' > /dev/null 2>&1
 
-echo "All done!"
+echo "Done!"
